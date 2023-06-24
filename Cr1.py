7e7ffabf40791999a65c80cac9525543dba0f2ec
Elham Fazel <55910811+fazelelham32@users.noreply.github.com>
2023-06-24 20:25:56 +0330

    Add files via upload

---
+++ Cr1.py
@@ -0,0 +1,66 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, World!")
+Hello, World!
+>>> 18/3
+6.0
+>>> 18/7
+2.5714285714285716
+>>> 18.0/7
+2.5714285714285716
+>>> 18.0/7.0
+2.5714285714285716
+>>> 18/7.
+2.5714285714285716
+>>> 9%4
+1
+>>> 8%4
+0
+>>> 8.75%.5
+0.25
+>>> 6*7
+42
+>>> 8**3
+512
+>>> -5**4
+-625
+>>> x = 18
+>>> x + 15
+33
+>>> x**3
+5832
+>>> y=54
+>>> x+y
+72
+>>> g=input("Enter number here: ")
+Enter number here: 43
+>>> g + 32
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    g + 32
+TypeError: can only concatenate str (not "int") to str
+>>> 5**4
+625
+>>> pow(5, 4)
+625
+>>> abs(5)
+5
+>>> abs(-18)
+18
+>>> import math
+>>> math.floor(18.7)
+18
+>>> math.sqrt(81)
+9.0
+>>> bucky = matj.sqrt
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    bucky = matj.sqrt
+NameError: name 'matj' is not defined
+>>> bucky=math.sqrt
+>>> bucky(9)
+3.0
+>>> bucky=math.floor
+>>> bucky(19.8)
+19
+>>> 
