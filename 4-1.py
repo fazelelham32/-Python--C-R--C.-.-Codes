5318b08e4d15d36ff547959408c5180ec10103e5
Elham Fazel <55910811+fazelelham32@users.noreply.github.com>
2024-08-31 00:14:08 +0330

    source code4-projects

---
+++ 4-1.py
@@ -0,0 +1,10 @@
+
+
+def main():
+    a,b=0,1
+    if(a<b):
+        print('This is true')
+    else:
+        print('This is false')
+   
+if __name__ == "__main__": main()
---
+++ 4-2.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    v="six"
+    if v=="one":
+        print("This is one")
+    elif(v=="two"):
+        print("This is two")
+    elif(v=="three"):
+        print("This is three")
+    else:
+        print("Other number")
+   
+if __name__ == "__main__": main()
