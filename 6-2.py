436e046a93138f1635faa926735892cdd8cafac8
Elham Fazel <55910811+fazelelham32@users.noreply.github.com>
2024-08-31 00:25:28 +0330

    source code6-projects

---
+++ 6-2.py
@@ -0,0 +1,17 @@
+
+# Created by Hamidreza Talebi
+
+import re
+
+def main():
+    fh = open('raven.txt')
+    for line in fh:
+            print(re.sub('(Len|Neverm)ore','###', line), end='') 
+    
+if __name__ == "__main__": main()
+
+
+
+
+
+
