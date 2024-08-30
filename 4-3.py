e083aef3291a9629a35b3ae30856564b1bb8e62d
Elham Fazel <55910811+fazelelham32@users.noreply.github.com>
2024-08-30 23:48:07 +0330

    Add files via upload

---
+++ 4-3.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    choices=dict(
+                 one='first',
+                 two='two',
+                 three='three'
+                 )
+    v='two'
+    print(choices[v])
+   
+if __name__ == "__main__": main()
